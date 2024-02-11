9988e246125594fe2fb4cf9636b5f8e74f928c39
Utente <Utente@PC-DI-SIMO>
2024-02-11 20:12:08 +0100

    Aggiungere i file di progetto.

---
+++ 24.19/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24._19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Vector v1 = Vector.Parse(textBox1.Text);
+            Vector v2 = Vector.Parse(textBox2.Text);    
+            Vector somma = v1 + v2;
+            label1.Text = somma.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Vector v1 = Vector.Parse(textBox1.Text);
+            Vector v2 = Vector.Parse((textBox2.Text));
+            Vector sottrazione = v1 - v2;
+            label1.Text = sottrazione.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Vector v1 = Vector.Parse(textBox1.Text);
+            Vector v2 = Vector.Parse(textBox2.Text);
+            Vector molt = v1 * v2;
+            label1.Text = molt.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Vector v1 = Vector.Parse(textBox1.Text);
+            int scalare = int.Parse(textBox2.Text);
+            Vector prodotto = scalare * v1;
+            label1.Text = prodotto.ToString();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int scalare = int.Parse((textBox1.Text));
+            Vector vector = Vector.Parse((textBox2.Text));
+            Vector prod = scalare * vector;
+            label1.Text = prod.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Vector vector = Vector.Parse((textBox1.Text));
+            Vector inv1 = -vector;
+            label1.Text = inv1.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Vector v = Vector.Parse((textBox2.Text));
+            Vector inv2 = -v;
+            label1.Text = inv2.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Vector v = Vector.Parse(textBox1.Text);
+            Vector check = +v;
+            label1.Text = check.ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Vector v = Vector.Parse(textBox2.Text);
+            Vector check = +v;
+            label1.Text = check.ToString();
+        }
+    }
+}
---
+++ 24.19/Vector.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._19
+{
+    internal class Vector
+    {
+        private readonly float x;
+        private readonly float y;
+
+        public float X { get{ return x; } }
+        public float Y { get { return y; } }
+
+        public Vector(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.X+b.X, a.Y+b.Y);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0};{1}", x, y);
+        }
+
+        //19.20
+
+        public static Vector operator +(Vector v)
+        {
+            return new Vector(v.x, v.y);
+        }
+
+        public static Vector operator -(Vector v)
+        {
+            return new Vector(v.y, v.x);
+        }
+
+        public static Vector operator-(Vector a, Vector b)
+        {
+            return new Vector(a.X-b.X, a.Y-b.Y);
+        }
+
+        //metodi richiesti per mail
+
+        public static Vector operator *(Vector a, Vector b)
+        {
+            return new Vector(a.x*b.x, a.y*b.y);
+        }
+
+        public static Vector operator *(float a, Vector b)
+        {
+            return new Vector(a*b.x, a*b.y);
+        }
+
+        public static Vector Parse(string s)
+        {
+            string[] strings = s.Split(';');
+            return new Vector(float.Parse(strings[0]), float.Parse(strings[1]));
+        }
+
+        
+    }
+}
